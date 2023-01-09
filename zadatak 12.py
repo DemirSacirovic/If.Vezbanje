c8fbc186f1b5746ec795bca4ba9bf7a781d7f678
DemirSacirovic <121728128+DemirSacirovic@users.noreply.github.com>
2023-01-09 19:20:37 +0100

    Add files via upload

---
+++ zadatak 12.py
@@ -0,0 +1,11 @@
+broj = int(input("Unesite ceo broj:"))
+if broj%2==0:
+    if broj>10:
+        print ("Broj koji ste uneli je paran")
+    else:
+        print ("Broj koji ste uneli je paran, i nije veći od 10.")
+else:
+    if broj>=5 and broj<=30:
+        print("Broj koji ste uneli nije paran i ne pripada opsegu od 5 do 30")
+    else:
+        print ("Broj koji ste uneli nije paran i ne pripada opsegu od 5 do 30")
